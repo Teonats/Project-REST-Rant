fbe00666f380d56139ec58d32554ade692b26b8e
Your Name <teonatskhadaia@yexample.com$ git config --global user.name teona>
2023-02-13 23:50:31 -0500

    Adding Bootstrap grid to index page

--- views/home.jsx
+++ views/home.jsx
@@ -4,12 +4,17 @@ const Def = require('./default')
 function home () {
     return (
       <Def>
-          <main>
-              <h1>HOME</h1>
+        <main>
+        <h1>HOME</h1>
+              <div>
+              <img src= "/images/sandwiches.jpg"   alt="Avocados Bananas Tomato Toast"/>
+              </div>
+              <div>
+                Photo by <a href="https://unsplash.com/@ellaolsson">Ella Olsson</a> on <a href="https://unsplash.com/s/photos/free-food">Unsplash</a>
+              </div>
               <a href="/places">
-          <button className="btn-primary">Places Page</button>
-           </a>
-
+                <button className="btn-primary">Places Page</button>
+              </a>
 
           </main>
       </Def>
