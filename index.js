759e7d57a464ae7ac0a75781479db6900a64acbc
Your Name <teonatskhadaia@yexample.com$ git config --global user.name teona>
2023-02-13 19:40:29 -0500

    Adding styles to Rest-rant

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
+//Moduls and Globals
 require('dotenv').config()
 const express = require('express')
 const app = express()
 
+//express Settings
 app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
+app.use(express.static('public'))
 
+//controllers & Routers
 app.use('/places', require('./controllers/places'))
+
+app.get('/', (req, res) => {
+    res.render('home')
+  })
 
 app.get("*", (req, res) => {
     res.render('error404')
