36b546242377574f3a16d36471b4b5f44528b5a6
Your Name <teonatskhadaia@yexample.com$ git config --global user.name teona>
2023-02-28 22:58:28 -0500

    adding to  populate data for star ratings

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -7,7 +7,24 @@ function show (data) {
       no comments yet!
     </h3>
   )
+  let rating = (
+    <h3 className="inactive">
+      Not yet rated
+    </h3>
+  )
   if (data.place.comments.length) {
+    let sumRatings = data.place.comments.reduce((tot, c) => {
+      return tot + c.stars
+    }, 0)
+    let averageRating = Math.round(sumRatings / data.place.comments.length)
+    let stars = ''
+    for (let i = 0; i < averageRating; i++)
+    stars += '⭐'
+    rating = (
+      <h3>
+       {stars} stars
+      </h3>
+    )
     comments = data.place.comments.map(c => {
       return (
         <div className="border">
@@ -17,6 +34,7 @@ function show (data) {
           <strong>- {c.author}</strong>
         </h3>
         <h4>Rating: {c.stars}</h4>
+        {rating}
         </div>
       )
     })
